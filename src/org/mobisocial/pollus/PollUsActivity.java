0fdfd7dc13d5d07a312b6e09907ba1b8f739ecb2
Ben Dodson <bjdodson@gmail.com>
2011-10-20 00:29:23 -0700

    4. Add basic UI.

--- src/org/mobisocial/pollus/PollUsActivity.java
+++ src/org/mobisocial/pollus/PollUsActivity.java
@@ -3,6 +3,7 @@ package org.mobisocial.pollus;
 import mobisocial.socialkit.musubi.Musubi;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PollUsActivity extends Activity {
     Musubi mMusubi;
@@ -12,7 +13,15 @@ public class PollUsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        findViewById(R.id.submit).setOnClickListener(mSubmitListener);
 
         mMusubi = Musubi.getInstance(this);
     }
+
+    private View.OnClickListener mSubmitListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
