44e388e8796ad0e0df221b33eba200722b09d7c0
Ben Dodson <bjdodson@gmail.com>
2011-10-20 00:15:11 -0700

    3. Add MobiSocial's SocialKit library.

--- src/org/mobisocial/pollus/PollUsActivity.java
+++ src/org/mobisocial/pollus/PollUsActivity.java
@@ -1,13 +1,18 @@
 package org.mobisocial.pollus;
 
+import mobisocial.socialkit.musubi.Musubi;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class PollUsActivity extends Activity {
+    Musubi mMusubi;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mMusubi = Musubi.getInstance(this);
     }
 }
